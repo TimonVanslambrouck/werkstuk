4e418371ac4c7af3feb855ab984aa79b0a9edb83
Timon Vanslambrouck <timon.vanslambrouck@student.ehb.be>
2021-11-26 15:43:38 +0100

    text change

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const server = express();
 const PORT = 3000;
 
 server.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}`);
+    console.log(`Server is now listening on ${PORT}`);
 })
 
 module.exports = server;
