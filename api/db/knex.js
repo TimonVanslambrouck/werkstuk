54883138793e034367b0be0d38eeaf1dd3340d25
Timon Vanslambrouck <timon.vanslambrouck@student.ehb.be>
2021-12-23 13:26:27 +0100

    feat(database): changed order added tables

--- api/db/knex.js
+++ api/db/knex.js
@@ -1,24 +1,25 @@
 const knex = require('knex')({
     client: 'pg',
+    version: '9.6',
     searchPath: ['knex', 'public'],
-    connection: process.env.POSTGRES_CONNECTION_STRING ? process.env.POSTGRES_CONNECTION_STRING : 'postgres://user:password@localhost:5432/postgress'
+    connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://example:example@localhost:5432/test'
 });
 
-knex.schema.hasTable('users').then(function (exists) {
-    if (!exists) {
-        return knex.schema.createTable('users', function (t) {
-            t.increments('id').primary();
-            t.string('username', 100).notNullable();
-            t.foreign('team_id').references('team.id').inTable('teams');
-            t.timestamps();
-        });
-    }
-});
 knex.schema.hasTable('teams').then(function (exists) {
     if (!exists) {
         return knex.schema.createTable('teams', function (t) {
             t.increments('team_id').primary();
             t.string('team', 100).notNullable();
+            t.timestamps();
+        });
+    }
+});
+knex.schema.hasTable('users').then(function (exists) {
+    if (!exists) {
+        return knex.schema.createTable('users', function (t) {
+            t.increments('id').primary();
+            t.string('username', 100).notNullable();
+            t.foreign('team_id').unsigned().references('team_id').inTable('teams');
             t.timestamps();
         });
     }
