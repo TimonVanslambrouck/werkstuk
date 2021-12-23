424590dc098bfaa32ab028e2a715b5d03b61d469
Timon Vanslambrouck <timon.vanslambrouck@student.ehb.be>
2021-12-23 16:05:04 +0100

    feat(tetsing): wrote checkUsername

--- api/helper/databaseHelper.js
+++ api/helper/databaseHelper.js
@@ -1,6 +1,12 @@
 function checkUsername(name) {
-
+    if (name.includes(" ") || name.match(/^[^a-zA-Z0-9]+$/) || name === '') {
+        return false;
+    } else {
+        return true;
+    }
 }
+
+// Source: https://stackoverflow.com/a/32311229
 
 module.exports = {
     checkUsername
