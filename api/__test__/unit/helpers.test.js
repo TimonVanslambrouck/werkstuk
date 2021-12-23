5f9c79b0ff6490042422cae3c89d8eecdafea6aa
Timon Vanslambrouck <timon.vanslambrouck@student.ehb.be>
2021-12-23 16:00:37 +0100

    feat(testing): added empty test

--- api/__test__/unit/helpers.test.js
+++ api/__test__/unit/helpers.test.js
@@ -1,11 +1,16 @@
 const HELPERFUNCTIONS = require('../../helper/databaseHelper');
 
-test('user cannot type any symbols as username', () => {
-    expect(HELPERFUNCTIONS.checkUsername('correctname')).toBeTruthy();
+test('user cannot use any symbols as username', () => {
+    expect(HELPERFUNCTIONS.checkUsername('correctname123')).toBeTruthy();
     expect(HELPERFUNCTIONS.checkUsername('//')).toBeFalsy();
 })
 
-test('user cannot type any spaces in their username', () => {
+test('user cannot use any spaces in their username', () => {
     expect(HELPERFUNCTIONS.checkUsername('correctname')).toBeTruthy();
     expect(HELPERFUNCTIONS.checkUsername('not correct name')).toBeFalsy();
+})
+
+test('user cannot use an empty username', () => {
+    expect(HELPERFUNCTIONS.checkUsername('correctname')).toBeTruthy();
+    expect(HELPERFUNCTIONS.checkUsername('')).toBeFalsy();
 })
