786f8018a374b45a41abd47f8ff34ef12cdf2987
Timon Vanslambrouck <timon.vanslambrouck@student.ehb.be>
2021-12-23 14:53:04 +0100

    feat(database): trying to insert data

--- api/db/knex.js
+++ api/db/knex.js
@@ -21,10 +21,15 @@ const knex = require('knex')({
                     return knex.schema.createTable('users', function (t) {
                         t.increments('id').primary();
                         t.string('username', 100).notNullable();
-                        t.foreign('team_id').unsigned().references('team_id').inTable('teams');
+                        t.foreign('team_id').unsigned();
+                        t.foreign('team_id').references('team_id').inTable('teams');
                         t.timestamps();
                     });
                 }
+            })
+        ).then(
+            knex('teams').insert({
+                team: 'Los Angeles Lakers',
             })
         )
 
